49557e5704f3f67ea9f1058794ddb1b1963e1c81
Aditya Raj Singh <ranawataaditya06@gmail.com>
2025-07-15 16:01:15 +0530

    Created another api endpoint for mentors.

---
+++ api/mentor.js
@@ -0,0 +1,33 @@
+import { users } from "./users.js";
+
+export default function handler(req, res) {
+    const { id } = req.query;
+    const mentorId = parseInt(id);
+
+    const mentors = users.find(u => u.role === "mentors")?.data;
+    const mentor = mentors.find(m => m.id === mentorId);
+
+    if(!mentor) {
+        return res.status(404).json({ error: "Mentor not found" });
+    }
+
+
+
+    const stats = {
+        interactedWithStudents: Math.floor(Math.random() * 100) + 10,
+        averageRating: (Math.random() * (5 - 3) + 3).toFixed(1),
+        subjectsTaught: {
+          Maths: Math.floor(Math.random() * 50),
+          Biology: Math.floor(Math.random() * 50),
+          Hindi: Math.floor(Math.random() * 50),
+          English: Math.floor(Math.random() * 50),
+          Physics: Math.floor(Math.random() * 50),
+          Chemistry: Math.floor(Math.random() * 50)
+        }
+      };
+
+    res.status(200).json({
+        ...mentor,
+        ...stats
+    });
+}
