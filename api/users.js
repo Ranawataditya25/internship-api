8d8b0644429023d2d28c8d555fe9f05d0d5986c4
Aditya Raj Singh <ranawataaditya06@gmail.com>
2025-07-20 15:49:53 +0530

    Solved cors error

--- api/users.js
+++ api/users.js
@@ -163,6 +163,16 @@ export const users = [
 
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight OPTIONS request
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+
   res.status(200).json(users);
 }
 
